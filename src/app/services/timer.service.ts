6ce65cebb1bac3374440b4fa6636e9e69bc9dc2c
sga101 <sga101@yahoo.co.uk>
2020-05-23 17:24:14 +0100

    remove invalid import

--- src/app/services/timer.service.ts
+++ src/app/services/timer.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, Subject, timer } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
-import { threadId } from 'worker_threads';
 
 @Injectable({
   providedIn: 'root'
