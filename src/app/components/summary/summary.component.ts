b7fe729d79a05570761c885049333eb6df9fe2bc
sga101 <sga101@yahoo.co.uk>
2020-05-23 10:28:30 +0100

    summary refactor part 1

--- src/app/components/summary/summary.component.ts
+++ src/app/components/summary/summary.component.ts
@@ -23,6 +23,30 @@ export class SummaryComponent implements OnChanges {
 
   constructor(private readonly cdr: ChangeDetectorRef) {}
 
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.questionHistory && !!this.questionHistory) {
+      const data: TableSummary[] = this.getSummaryForAllTables(this.questionHistory);
+      this.summaryData = data;
+      this.cdr.markForCheck();
+    }
+  }
+
+  private getSummaryForAllTables(questions: Question[]): TableSummary[] {
+    const tables = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    const data = tables
+      .map((table) =>
+        this.getSummary(
+          table,
+          questions.filter((q) => q.y === table)
+        )
+      )
+      .filter((q) => q.answered > 0)
+      .sort((a, b) => b.averageTimePerQuestion - a.averageTimePerQuestion);
+
+    data.push(this.getSummary(0, questions));
+    return data;
+  }
+
   getSummary(table: number, questions: Question[]): TableSummary {
     const result: TableSummary = {
       table,
@@ -34,29 +58,14 @@ export class SummaryComponent implements OnChanges {
 
     result.answered = questions.length;
     if (result.answered > 0) {
-      result.totalTime = questions.reduce((acc, curr) => acc + curr.endTime - curr.startTime, 0);
+      result.totalTime = questions.reduce((total, question) => total + this.getTimeForQuestion(question), 0);
       result.averageTimePerQuestion = result.totalTime / result.answered;
       result.rightFirstTime = questions.filter((q) => q.answers.length === 1).length;
     }
     return result;
   }
-  ngOnChanges(changes: SimpleChanges): void {
-    if (changes.questionHistory && !!this.questionHistory) {
-      let data: TableSummary[] = [];
 
-      for (let i = 1; i < 13; i++) {
-        data.push(
-          this.getSummary(
-            i,
-            this.questionHistory.filter((q) => q.y === i)
-          )
-        );
-      }
-      data = data.sort((a, b) => b.averageTimePerQuestion - a.averageTimePerQuestion);
-
-      data.push(this.getSummary(0, this.questionHistory));
-      this.summaryData = data;
-      this.cdr.markForCheck();
-    }
+  getTimeForQuestion(question: Question): number {
+    return question.endTime - question.startTime;
   }
 }
