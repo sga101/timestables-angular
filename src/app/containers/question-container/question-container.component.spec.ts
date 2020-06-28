5be9d1e1abf1291eca4c85663a25002dcc004cb8
sga101 <sga101@outlook.com>
2020-06-28 09:51:20 +0100

    Make tests pass

--- src/app/containers/question-container/question-container.component.spec.ts
+++ src/app/containers/question-container/question-container.component.spec.ts
@@ -1,7 +1,17 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatCardModule } from '@angular/material/card';
+import { CalculationsService } from 'src/app/services/calculations.service';
+import { GameService } from 'src/app/services/game.service';
+import { HistoryService } from 'src/app/services/history.service';
+import { MultiChoiceAnswersService } from 'src/app/services/multi-choice-answers.service';
+import { QuestionService } from 'src/app/services/question.service';
+import { RandomNumbersService } from 'src/app/services/random-numbers.service';
+import { ResultsService } from 'src/app/services/results.service';
+import { SummaryService } from 'src/app/services/summary.service';
+import { TableSelectionService } from 'src/app/services/table-selection.service';
+import { TimerService } from 'src/app/services/timer.service';
 import { QuestionContainerComponent } from './question-container.component';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('QuestionContainerComponent', () => {
   let component: QuestionContainerComponent;
@@ -11,6 +21,18 @@ describe('QuestionContainerComponent', () => {
     TestBed.configureTestingModule({
       imports: [MatCardModule],
       declarations: [QuestionContainerComponent],
+      providers: [
+        QuestionService,
+        SummaryService,
+        ResultsService,
+        HistoryService,
+        GameService,
+        MultiChoiceAnswersService,
+        TimerService,
+        RandomNumbersService,
+        TableSelectionService,
+        CalculationsService
+      ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
   }));
