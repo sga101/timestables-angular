cac354c767faab223899b772a80423364d0e7a09
sga101 <sga101@outlook.com>
2022-02-19 15:54:41 +0000

    remove unused component + fix test warnings

--- src/app/components/keyed-in-answer/when-dirty-and-invalid-matcher.ts
+++
@@ -1,9 +0,0 @@
-import { FormControl } from '@angular/forms';
-import { ErrorStateMatcher } from '@angular/material/core';
-
-/** Error when invalid control is dirty, touched, or submitted. */
-export class WhenDirtyAndInvalidMatcher implements ErrorStateMatcher {
-  isErrorState(control: FormControl | null): boolean {
-    return !!(control && control.invalid && (control.dirty || control.touched));
-  }
-}
--- src/app/components/setup-game/setup-game.component.spec.ts
+++ src/app/components/setup-game/setup-game.component.spec.ts
@@ -1,15 +1,24 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatTableModule } from '@angular/material/table';
+import { TableChooserComponent } from '../table-chooser/table-chooser.component';
 import { SetupGameComponent } from './setup-game.component';
 
 describe('SetupGameComponent', () => {
   let component: SetupGameComponent;
   let fixture: ComponentFixture<SetupGameComponent>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [SetupGameComponent]
-    }).compileComponents();
-  }));
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [SetupGameComponent, TableChooserComponent],
+        imports: [FormsModule, MatButtonModule, MatRadioModule, MatSlideToggleModule, MatTableModule]
+      }).compileComponents();
+    })
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SetupGameComponent);
