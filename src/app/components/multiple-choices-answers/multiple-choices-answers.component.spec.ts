ba3da20237a2d9635d4c3d5eced79dfe39658459
sga101 <sga101@outlook.com>
2022-03-26 17:22:38 +0000

    tests for multiple choices component

--- src/app/components/multiple-choices-answers/multiple-choices-answers.component.spec.ts
+++ src/app/components/multiple-choices-answers/multiple-choices-answers.component.spec.ts
@@ -1,6 +1,24 @@
+import { Component, Input } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatButtonModule } from '@angular/material/button';
+import { Question } from 'src/app/models/question.model';
 import { MultipleChoicesAnswersComponent } from './multiple-choices-answers.component';
+
+@Component({
+  selector: 'app-question',
+  template: '<span class="x">{{question.x}}</span><span class="y">{{question.y}}</span>'
+})
+export class MockQuestionComponent {
+  @Input() question: Question;
+}
+
+@Component({
+  selector: 'app-answer-feedback',
+  template: '<span id="isCorrect">{{question.answeredCorrectly}}</span>'
+})
+export class MockAnswerFeedbackComponent {
+  @Input() question: Question;
+}
 
 describe('MultipleChoicesAnswersComponent', () => {
   let component: MultipleChoicesAnswersComponent;
@@ -9,17 +27,44 @@ describe('MultipleChoicesAnswersComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [MatButtonModule],
-      declarations: [MultipleChoicesAnswersComponent]
+      declarations: [MultipleChoicesAnswersComponent, MockQuestionComponent, MockAnswerFeedbackComponent]
     }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(MultipleChoicesAnswersComponent);
     component = fixture.componentInstance;
+    component.choices = [1, 2, 3, 4];
+    const x = 1;
+    const y = 2;
+    const startTime = Date.now();
+
+    component.question = {
+      x,
+      y,
+      startTime,
+      answers: [],
+      endTime: 0,
+      answered: false,
+      answeredCorrectly: false,
+      currentAnswer: ''
+    };
+
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should render the question', () => {
+    const x = fixture.nativeElement.querySelector('.x');
+    const y = fixture.nativeElement.querySelector('.y');
+    expect(x.textContent).toEqual('1');
+    expect(y.textContent).toEqual('2');
+  });
+
+  it('should emit the answer', (done) => {
+    const sub = component.answeredQuestion.subscribe((a) => {
+      expect(a).toEqual(1);
+      done();
+    });
+    component.answerQuestion('1');
   });
 });
