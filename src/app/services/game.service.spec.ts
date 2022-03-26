05d5b8317d5c83436984f73cd57813163ec0af31
sga101 <sga101@outlook.com>
2022-03-26 17:54:13 +0000

    game service tests

--- src/app/services/game.service.spec.ts
+++ src/app/services/game.service.spec.ts
@@ -1,16 +1,91 @@
-import { TestBed } from '@angular/core/testing';
-
+import { filter, map, pairwise, take, tap } from 'rxjs';
+import { CalculationsService } from './calculations.service';
 import { GameService } from './game.service';
+import { HistoryService } from './history.service';
+import { QuestionService } from './question.service';
+import { RandomNumbersService } from './random-numbers.service';
+import { ResultsService } from './results.service';
+import { TableSelectionService } from './table-selection.service';
+import { TimerService } from './timer.service';
 
 describe('GameService', () => {
   let service: GameService;
+  let questionService: QuestionService;
+  let resultsService: ResultsService;
+  let historyService: HistoryService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(GameService);
+    historyService = new HistoryService();
+    questionService = new QuestionService(new TimerService(), new RandomNumbersService(), new TableSelectionService());
+    resultsService = new ResultsService(new CalculationsService(), historyService);
+    service = new GameService(questionService, resultsService, historyService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should start in setup mode', (done) => {
+    service.gameStatus$.subscribe((status) => {
+      expect(status).toEqual('Setup');
+      done();
+    });
+  });
+
+  it('should set status = playing after reset', (done) => {
+    service.reset(1);
+    service.gameStatus$.subscribe((status) => {
+      expect(status).toEqual('Playing');
+      done();
+    });
+  });
+
+  it('should end game after last question is answered correctly', (done) => {
+    // automatically answer all the question
+    questionService.questions$
+      .pipe(
+        filter((question) => question && !question.answered),
+        map((question) => questionService.answerQuestion(question.x * question.y))
+      )
+      .subscribe();
+
+    // monitor the game status
+    service.gameStatus$
+      .pipe(
+        filter((gameStatus) => gameStatus === 'Finished'),
+        map(() => done())
+      )
+      .subscribe();
+
+    // start a game with 2 questions
+    service.reset(2);
+  });
+
+  it('should change multichoice mode when requested', (done) => {
+    service.isMultiChoice$
+      .pipe(
+        pairwise(),
+        map(([first, second]) => {
+          expect(first).toBeTruthy();
+          expect(second).toBeFalsy();
+          done();
+        })
+      )
+      .subscribe();
+    service.setMultiChoiceMode(true);
+    service.setMultiChoiceMode(false);
+
+    service.setMultiChoiceMode(true);
+  });
+
+  it('should revert to setup when changeSettings is called', (done) => {
+    service.gameStatus$
+      .pipe(
+        pairwise(),
+        filter(([first, second]) => first === 'Playing' && second === 'Setup'),
+        take(1),
+        map(() => {
+          done();
+        })
+      )
+      .subscribe();
+    service.reset(1);
+    service.changeSettings();
   });
 });
