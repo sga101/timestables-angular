aa6404d0285e2037173b52b926ecda94c3f5cee2
Simon Arthur <sgarthur@gmail.com>
2026-01-11 17:11:34 +0000

    fix tests

--- src/setupjest.ts
+++ src/setupjest.ts
@@ -9,21 +9,3 @@ const mock = () => {
     clear: () => (storage = {}),
   };
 };
-
-Object.defineProperty(window, 'localStorage', { value: mock() });
-Object.defineProperty(window, 'sessionStorage', { value: mock() });
-Object.defineProperty(window, 'getComputedStyle', {
-  value: () => ['-webkit-appearance'],
-});
-
-Object.defineProperty(document.body.style, 'transform', {
-  value: () => {
-    return {
-      enumerable: true,
-      configurable: true,
-    };
-  },
-});
-
-/* output shorter and more meaningful Zone error stack traces */
-// Error.stackTraceLimit = 2;
